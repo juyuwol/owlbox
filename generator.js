30c74c8039fbd1dc269a1b693289c7afe0155f94
juyuwol <ju@yuwol.pe.kr>
2025-11-03 18:26:27 +0900

    Fast fix

--- generator.js
+++ generator.js
@@ -185,6 +185,15 @@ form.onsubmit = (event) => {
   const frameColorHex = hex(frameColor);
   const textColorHex = hex(textColor);
   const text = output.value = `\
+export const colors = {
+  빨간색: [0xdd, 0x2e, 0x44], // #dd2e44
+  주황색: [0xf4, 0x90, 0x0c], // #f4900c
+  노란색: [0xfd, 0xcb, 0x58], // #fdcb58
+  초록색: [0x78, 0xb1, 0x59], // #78b159
+  파란색: [0x5d, 0xad, 0xec], // #5dadec
+  보라색: [0xaa, 0x8e, 0xd6], // #aa8ed6
+};
+
 export const fonts = [${[...fontMap.keys()].reduce((code, name) => code + `
   ${JSON.stringify(name)},`, '')}
 ];
